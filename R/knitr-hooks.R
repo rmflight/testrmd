9b389bcdf595ca55e72c588b2a822f34bd150573
Joe Cheng <joe@rstudio.com>
2017-05-26 09:38:57 -0700

    Track error counts
    (Attempted to track pass counts but it isn't working)

--- R/knitr-hooks.R
+++ R/knitr-hooks.R
@@ -13,8 +13,27 @@ init <- function() {
     }
   )
 
-  knitr::knit_hooks$set(chunk = knitr_chunk_hook)
+  error <- knitr_error_hook(knitr::knit_hooks$get("error"))
+  evaluate <- knitr_evaluate_hook(knitr::knit_hooks$get("evaluate"))
+  knitr::knit_hooks$set(
+    chunk = knitr_chunk_hook,
+    error = error
+  )
 }
+
+increment_count <- function(type) {
+  (current_chunk_counts[[type]] <- current_chunk_counts[[type]] + 1L)
+}
+reset_counts <- function() {
+  current_chunk_counts$error <- 0
+  current_chunk_counts$pass <- 0
+}
+get_count <- function(type) {
+  current_chunk_counts[[type]]
+}
+
+current_chunk_counts <- new.env(parent = emptyenv())
+reset_counts()
 
 #' @export
 styles <- function() {
@@ -36,15 +55,45 @@ render_template <- function(template_name, data) {
   whisker::whisker.render(template, data)
 }
 
+knitr_error_hook <- function(old_hook) {
+  force(old_hook)
+  function(x, options) {
+    if (isTRUE(options$test)) {
+      increment_count("error")
+    }
+    old_hook(x, options)
+  }
+}
+
+knitr_evaluate_hook <- function(old_hook) {
+  force(old_hook)
+  function(...) {
+    withCallingHandlers(
+      old_hook(...),
+      expectation_success = function(e) {
+        increment_count("pass")
+      }
+    )
+  }
+}
+
 knitr_chunk_hook <- function(x, options) {
   if (!isTRUE(options$test)) {
     return(x)
   }
 
+  on.exit(reset_counts(), add = TRUE)
+
+  pass <- get_count("error") == 0
+
   data <- list(
     chunk_id = sprintf("testrmd-chunk-%07d", sample.int(9999999, 1)),
     button_class = "default",
-    status = "pass"
+    bootstrap_class = if (pass) "success" else "danger",
+    status = if (pass) "pass" else "fail",
+    pass = pass,
+    pass_count = get_count("pass"),
+    error_count = get_count("error")
   )
   begin <- render_template("chunk-begin", data)
   end <- render_template("chunk-end", data)
