f4c00e04a7f91afed616be69fe65898b2910b302
Robert M Flight <r.flight79@gmail.com>
2017-05-26 10:53:01 -0700

    check whether or not it is interactive or not for logging.

--- R/logging-errors.R
+++ R/logging-errors.R
@@ -1,17 +1,25 @@
 log_env <- new.env(parent = emptyenv())
 
 init_log_file <- function(){
-  log_env$log_file <- file.path(getwd(), "testrmd_errors.log")
-  cat(as.character(Sys.time()), file = log_env$log_file, sep = "\n", append = FALSE)
+  if (!interactive()) {
+    log_env$do_logging <- TRUE
+    log_env$log_file <- file.path(getwd(), "testrmd_errors.log")
+    cat(as.character(Sys.time()), file = log_env$log_file, sep = "\n", append = FALSE)
+  } else {
+    log_env$do_logging <- FALSE
+  }
 }
 
 write_to_log <- function(x, options){
-  error_code <- options$code
-  error_message <- gsub("##", "", x)
-  error_message <- gsub("\n", "", error_message)
+  if (log_env$do_logging) {
+    error_code <- options$code
+    error_message <- gsub("##", "", x)
+    error_message <- gsub("\n", "", error_message)
 
-  #browser(expr = TRUE)
+    #browser(expr = TRUE)
 
-  log_error <- paste0(error_message, " :: ", error_code)
-  cat(log_error, file = log_env$log_file, sep = "\n", append = TRUE)
+    log_error <- paste0(error_message, " :: ", error_code)
+    cat(log_error, file = log_env$log_file, sep = "\n", append = TRUE)
+  }
+
 }
